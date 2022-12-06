ccf8f459d0afd98c5adf7def51a18696721a49af
Nate Sweet <nathanjsweet@pm.me>
2022-12-06 13:18:21 -0800

    Fix "timeafter" Tests, Add Missing Range Statement
    Timeafter tests were not checking for any positive cases
    and not using the "want" annotation.
    The for-range statement was not being vetted at all, this is
    fixed as well.
    
    Signed-off-by: Nate Sweet <nathanjsweet@pm.me>

--- analysis/timeafter/testdata/time_after_not_in_for_loop.go
+++ analysis/timeafter/testdata/time_after_not_in_for_loop.go
@@ -14,7 +14,10 @@
 
 package testdata
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func TimeAfterNotInForLoop() {
 	<-time.After(time.Millisecond)
@@ -23,5 +26,19 @@ func TimeAfterNotInForLoop() {
 func TimeAfterNotInForLoop2() {
 	select {
 	case <-time.After(time.Millisecond):
+	}
+}
+
+func TimeAfterInForLoop() {
+	for i, l := 0, 3; i < l; i++ {
+		fmt.Printf("time_after_perumutation_%d", i)
+		<-time.After(time.Millisecond) // want `use of time.After in a for loop is prohibited, use inctimer instead`
+	}
+}
+
+func TimeAfterInForRangeLoop() {
+	for _, n := range []int{0, 1, 2} {
+		fmt.Printf("time_after_perumutation_%d", n)
+		<-time.After(time.Millisecond) // want `use of time.After in a for loop is prohibited, use inctimer instead`
 	}
 }
